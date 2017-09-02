6edfe71eb6bcb1554c4c291c3a627aa9815a0b75
Gareth Nelson <gareth@steemit.com>
2017-09-02 18:55:42 +0200

    add a couple more tests

--- test/sanitizer.ts
+++ test/sanitizer.ts
@@ -5,9 +5,19 @@ import {sanitize} from '../src/'
 
 describe('Sanitizer', function() {
 
-    it('should sanitize', async function() {
+    it('should basic sanitize', async function() {
         const dirty = '<b class="boring">Hi! <script>alert("hax")</script></b>'
         assert.equal(sanitize(dirty), '<b>Hi! </b>')
     })
 
+
+    it('should strip script tag with hex space', async function() {
+        const dirty = '<script\x20type="text/javascript">javascript:alert(1);</script>'
+        assert.equal(sanitize(dirty),'')
+    })
+
+    it('should strip script tag with fake close', async function() {
+        const dirty = '\'`"><\x3Cscript>javascript:alert(1)</script>'
+        assert.equal(sanitize(dirty).indexOf('script'),-1)
+    })
 })
