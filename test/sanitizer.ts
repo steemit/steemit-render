dcd31674865515f45cdfb5e1f10ed9ba1d101b4f
Gareth Nelson <gareth@steemit.com>
2017-09-02 19:02:35 +0200

    more tests

--- test/sanitizer.ts
+++ test/sanitizer.ts
@@ -20,4 +20,24 @@ describe('Sanitizer', function() {
         const dirty = '\'`"><\x3Cscript>javascript:alert(1)</script>'
         assert.equal(sanitize(dirty).indexOf('script'),-1)
     })
+
+    it('should strip img tag with onerror', async function() {
+        const dirty = '<img src=1 href=1 onerror="javascript:alert(1)"></img>'
+	assert.equal(sanitize(dirty),'<img src="brokenimg.jpg" />')
+    })
+    
+    it('should strip audio tag with onerror', async function() {
+        const dirty = '<audio src=1 href=1 onerror="javascript:alert(1)"></audio>'
+	assert.equal(sanitize(dirty),'')
+    })
+    
+    it('should strip commented script tag', async function() {
+        const dirty = '<!--\x3E<img src=xxx:x onerror=javascript:alert(1)> -->'
+	assert.equal(sanitize(dirty),'')
+    })
+
+    it('should strip link to javascript', async function() {
+        const dirty = '<a href="javascript\x3Ajavascript:alert(1)" id="fuzzelement1">test</a>'
+	assert.equal(sanitize(dirty),'<a rel="noopener">test</a>')
+    })
 })
